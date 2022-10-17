5a4971e952d7fb7a742e78aa5850c08032c5c745
Nixo371 <nicolas.ucieda@gmail.com>
2022-10-17 12:54:22 +0200

    all working

--- client.c
+++ client.c
@@ -3,8 +3,36 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
+static void ft_kill(int	pid, char *str)
+{
+	int	i;
+	char	c;
+
+	while (*str)
+	{
+		i = 8;
+		c = *str++;
+		while(i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR1); // Send a 1
+			else
+				kill(pid, SIGUSR2); // Send a 0
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR2);
+		usleep(100);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 3 || !ft_strlen(argv[2]))
 		return (1);
+
+	ft_kill(ft_atoi(argv[1]), argv[2]);
 }
--- server.c
+++ server.c
@@ -2,57 +2,33 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-static void action(int sig, siginfo_t *info, void *context)
+static void sig_handler(int signum)
 {
-	int				i;
-	unsigned char	c;
-	pid_t			client_pid; 
+	static unsigned char	c = 0;
+	static int				i = 0;
 
-	i = 0;
-	c = 0;
-	client_pid = 0;
-	(void)context;
-	if (!client_pid)
-	{
-		client_pid = info -> si_pid;
-	}
-	if (sig == SIGUSR2)
-	{
-		c = 1;
-	}
+	c |= (signum == SIGUSR1); //Make current bit a "1" is SIGUSR1 is passed
 	if (++i == 8)
 	{
 		i = 0;
-		if (!c)
-		{
-			kill(client_pid, SIGUSR2);
-			client_pid = 0;
-			return;
-		}
 		ft_putchar_fd(c, 1);
 		c = 0;
-		kill(client_pid, SIGUSR1);
 	}
 	else
-	{
 		c <<= 1;
-	}
 }
 
 int main(void)
 {
-	struct sigaction	s_sigaction;
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
 
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd('\n', 1);
-	s_sigaction.sa_sigaction = action;
-	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
+	ft_putstr_fd("Server PID: ", STDOUT_FILENO);
+	ft_putnbr_fd(getpid(), STDOUT_FILENO);
+	ft_putchar_fd('\n', STDOUT_FILENO);
 	while(1)
 	{
 		pause();
-	}
+	}	
 	return (0);
 }
